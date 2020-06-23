402ba3969615eebd701ab4fe159b12865543cc0a
Mokadev-vn <nhantu1001@gmail.com>
2020-06-23 18:16:08 +0700

    Push Assigment lan 1

---
+++ public/js/app.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+
+    $('#cart').hover(function () {
+        $('#list-cart').show(function(){
+            $('#list-cart').slideDown();
+        });
+    },function(){
+        $('#list-cart').hide();
+    });
+
+    $('#search').hover(function(){
+        $("#search-form").show(function(){
+        })
+    },function(){
+        $("#search-form").hide();
+    });
+
+    $('#slider-home').owlCarousel({
+        autoPlay: true,
+        navigation: true,
+        navigationText: true,
+        paginationNumbers: false,
+        stopOnHover: true,
+        items: 1 ,//10 items above 1000px browser widt
+        itemsDesktop: [770, 1], //5 items between 1000px and 901px
+        itemsDesktopSmall: false , // betweem 900px and 601px
+        itemsTablet: false , //2 items between 600 and 0
+        itemsMobile: [375, 1] // itemsMobile disabled - inherit from itemsTablet option
+    })
+
+    $('#product-new').owlCarousel({
+        autoPlay: true,
+        navigation: true,
+        navigationText: true,
+        paginationNumbers: false,
+        stopOnHover: true,
+        pagination : false,
+        navigationText: ['<i class="fa fa-chevron-left" aria-hidden="true"></i>','<i class="fa fa-chevron-right" aria-hidden="true"></i>'],
+        items: 4, //10 items above 1000px browser width
+        itemsDesktop: [770, 3] , //5 items between 1000px and 901px
+        itemsDesktopSmall: [600,2] , // betweem 900px and 601px
+        itemsTablet: false, //2 items between 600 and 0
+        itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+    })
+
+    $('#post-new').owlCarousel({
+        autoPlay: true,
+        navigation: true,
+        navigationText: false ,
+        paginationNumbers: false,
+        stopOnHover: true,
+        pagination : false,
+        items: 3, //10 items above 1000px browser width
+        itemsDesktop: false , //5 items between 1000px and 901px
+        itemsDesktopSmall: [600, 2] , // betweem 900px and 601px
+        itemsTablet: false, //2 items between 600 and 0
+        itemsMobile: false // itemsMobile disabled - inherit from itemsTablet option
+    })
+
+});
